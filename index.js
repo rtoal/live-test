953c72b809f3a6d589e2f6950a0f05f250056f4e
Ray Toal <rtoal@lmu.edu>
2016-11-27 15:04:52 -0800

    Use textareas for tests

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 (function () {
-  
+
+  const addTestButton = document.getElementById("addtest");
+  const testList = document.getElementById("testlist");
+
+  function addTest() {
+    let newTest = document.createElement("textarea");
+    newTest.innerHTML = '// Test';
+    testList.appendChild(newTest);
+  }
+  addTestButton.onclick = addTest;
 }());
