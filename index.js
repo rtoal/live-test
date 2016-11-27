fe78b5717b2120d73cf43bee199367580dd2c09b
Ray Toal <rtoal@lmu.edu>
2016-11-27 15:16:05 -0800

    Autoresizing text boxes!

--- index.js
+++ index.js
@@ -6,7 +6,17 @@
   function addTest() {
     let newTest = document.createElement("textarea");
     newTest.innerHTML = '// Test';
+    newTest.addEventListener('input', autoresize, false);
     testList.appendChild(newTest);
   }
+
+  function autoresize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight+'px';
+    this.scrollTop = this.scrollHeight;
+    window.scrollTo(window.scrollLeft,(this.scrollTop + this.scrollHeight));
+  }
+
   addTestButton.onclick = addTest;
+  addTest();
 }());
