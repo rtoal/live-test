58e0c8c3b912432f82b4e1802f07e46f53f6c94d
Ray Toal <rtoal@lmu.edu>
2016-11-27 19:03:30 -0800

    Add Ace Editor

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 (function () {
 
+  const PASS_COLOR = "green";
+  const FAIL_COLOR = "red";
+
   const addTestButton = document.getElementById("addtest");
   const testList = document.getElementById("testlist");
 
   function addTest() {
     let newTest = document.createElement("textarea");
-    newTest.innerHTML = '// Test';
-    newTest.addEventListener('input', autoresize, false);
+    newTest.innerHTML = '// Describe test here';
+    newTest.addEventListener('input', runTest, false);
     testList.appendChild(newTest);
+    runTest.call(newTest);
   }
 
-  function autoresize() {
+  function runTest() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight+'px';
     this.scrollTop = this.scrollHeight;
     window.scrollTo(window.scrollLeft,(this.scrollTop + this.scrollHeight));
+    evalTest(this);
+  }
+
+  function evalTest(textarea) {
+    try {
+      let pass = eval(textarea.value);
+      textarea.style.borderLeftColor = pass ? PASS_COLOR : FAIL_COLOR;
+    } catch (e) {
+      textarea.style.borderLeftColor = FAIL_COLOR;
+    }
   }
 
   addTestButton.onclick = addTest;
