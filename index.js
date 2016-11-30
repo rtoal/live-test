068e779591dc53029924ba54fe562ad59ba65b97
Ray Toal <rtoal@lmu.edu>
2016-11-29 16:44:28 -0800

    Support a setup to be run before all tests

--- index.js
+++ index.js
@@ -3,26 +3,32 @@
   const PASS_COLOR = "green";
   const FAIL_COLOR = "red";
 
+  const addSetupButton = document.querySelector(".addsetup");
   const addTestButton = document.querySelector(".addtest");
   const testList = document.getElementById("testlist");
+  let setupArea;
 
-  function addDeleteableTextArea(container) {
+  // Creates a div with a text area and a close button, returns the textarea.
+  // You can give a callback to do something after removing the div.
+  function addDeleteableTextArea(container, callback) {
     let div = document.createElement("div");
     let textArea = document.createElement("textarea");
-    let closeRegion = document.createElement("span");
-    closeRegion.className = "deletetest";
-    closeRegion.innerHTML = "&#x2296;";
+    let closer = document.createElement("span");
+    closer.className = "deletebutton";
+    closer.innerHTML = "&#x2296;";
     div.appendChild(textArea);
-    div.appendChild(closeRegion);
-    closeRegion.onclick = () => div.remove();
+    div.appendChild(closer);
+    closer.addEventListener('click', () => div.remove());
+    if (callback) closer.addEventListener('click', callback);
     container.appendChild(div);
     return textArea;
   }
 
+  // Adds a new text area for tests. Shift+Enter also creates a new one.
   function addTest() {
     let textArea = addDeleteableTextArea(testList);
     textArea.setAttribute('placeholder', 'Write test code here. Last expression is the assertion.');
-    textArea.addEventListener('input', runTest, false);
+    textArea.addEventListener('input', () => {sizeBox(textArea); evalTest(textArea);});
     textArea.addEventListener('keydown', e => {
       if (e.keyCode === 13 && e.shiftKey) {
         addTest();
@@ -33,17 +39,17 @@
     textArea.focus();
   }
 
-  function runTest() {
-    this.style.height = 'auto';
-    this.style.height = this.scrollHeight + 'px';
-    this.scrollTop = this.scrollHeight;
-    window.scrollTo(window.scrollLeft, (this.scrollTop + this.scrollHeight));
-    evalTest(this);
+  function sizeBox(box) {
+    box.style.height = 'auto';
+    box.style.height = box.scrollHeight + 'px';
+    box.scrollTop = box.scrollHeight;
+    window.scrollTo(window.scrollLeft, (box.scrollTop + box.scrollHeight));
   }
 
   function evalTest(textarea) {
+    let before = setupArea ? setupArea.value : '';
     try {
-      let pass = eval(editor.getValue() + ";" + textarea.value);
+      let pass = eval(editor.getValue() + "\n;" + before + "\n;" + textarea.value);
       textarea.style.borderLeftColor = pass ? PASS_COLOR : FAIL_COLOR;
     } catch (e) {
       textarea.style.borderLeftColor = FAIL_COLOR;
@@ -56,8 +62,16 @@
     }
   }
 
-  editor.getSession().on('change', runAllTests);
+  addSetupButton.onclick = () => {
+    setupArea = addDeleteableTextArea(document.getElementById('beforewrapper'), () => {
+      addSetupButton.style.display = 'inline';
+    });
+    setupArea.addEventListener('input', () => {sizeBox(setupArea); runAllTests();});
+    addSetupButton.style.display = 'none'
+  };
 
   addTestButton.onclick = addTest;
   addTest();
+
+  editor.getSession().on('change', runAllTests);
 }());
