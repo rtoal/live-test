c5b159c16e626fdc7d7c7252607984035d959411
Ray Toal <rtoal@lmu.edu>
2016-12-01 12:18:37 -0800

    Add save to local storage

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 (function () {
 
+  function $(selector) {return document.querySelector(selector);}
+  function $$(selector) {return Array.from(document.querySelectorAll(selector));}
+
   const PASS_COLOR = "green";
   const FAIL_COLOR = "red";
 
-  const addSetupButton = document.querySelector(".addsetup");
-  const addTestButton = document.querySelector(".addtest");
-  const testContainer = document.getElementById("testwrapper");
-  const beforeContainer = document.getElementById("beforewrapper");
+  const beforeContainer = $("#beforewrapper");
   const setupArea = addDeleteableTextArea(beforeContainer, hideSetup);
 
   // Creates a div with a text area and a close button, returns the textarea.
@@ -26,7 +26,7 @@
 
   // Adds a new text area for tests. Shift+Enter also creates a new one.
   function addTest() {
-    let textArea = addDeleteableTextArea(testContainer, div => div.remove());
+    let textArea = addDeleteableTextArea($("#testwrapper"), div => div.remove());
     textArea.setAttribute('placeholder', 'Write a test here. Last expression is the assertion.');
     textArea.addEventListener('input', () => {sizeBox(textArea); evalTest(textArea);});
     textArea.addEventListener('keydown', e => {
@@ -67,34 +67,52 @@
   }
 
   function runAllTests() {
-    for (let test of document.querySelectorAll("#testwrapper textarea")) {
-      evalTest(test);
-    }
+    $$("#testwrapper textarea").forEach(test => evalTest(test));
   }
 
   function showSetup() {
     beforeContainer.style.display = 'block';
-    addSetupButton.style.display = 'none';
+    $('.addsetup').style.display = 'none';
     runAllTests();
   }
 
   function hideSetup() {
     beforeContainer.style.display = 'none';
-    addSetupButton.style.display = 'inline';
+    $('.addsetup').style.display = 'inline';
     runAllTests();
+  }
+
+  function load(key) {
+    let bundle = localStorage.getItem('livetdd-' + key, JSON.stringify(bundle));
+    // TODO - validate item
+    editor.setValue(bundle.code);
+    // TODO - add setup
+    // TODO - add tests
+  }
+
+  function save() {
+    console.log('woo')
+    let key = prompt('Enter a local storage key at which to save your work');
+    if (!key) return;
+    let bundle = {
+      setup: beforeContainer.style.display === 'none' ? null : setupArea.value,
+      tests: $$("#testwrapper textarea").map(area => area.value),
+      code: editor.getValue()
+    };
+    localStorage.setItem('livetdd-' + key, JSON.stringify(bundle));
   }
 
   setupArea.setAttribute('placeholder', 'Add code to be run before every test here.');
   setupArea.addEventListener('input', () => {sizeBox(setupArea); runAllTests();});
-  addSetupButton.addEventListener('click', showSetup);
-  addTestButton.addEventListener('click', addTest);
+  $('.addsetup').addEventListener('click', showSetup);
+  $('.addtest').addEventListener('click', addTest);
   hideSetup();
   addTest();
 
-  let helpModal = document.querySelector(".modal");
+  let helpModal = $(".modal");
   helpModal.onclick = () => helpModal.style.display = 'none';
-  document.querySelector("#helpbutton").addEventListener('click', () => {
-    helpModal.style.display = 'block';
-  });
+  $("#helpbutton").addEventListener('click', () => {helpModal.style.display = 'block';});
+  $("#loadbutton").addEventListener('click', load);
+  $("#savebutton").addEventListener('click', save);
   editor.getSession().on('change', runAllTests);
 }());
