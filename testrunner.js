61269ed30d4a479e7c8aa89f40e2b8375b5ccc3b
Ray Toal <rtoal@lmu.edu>
2016-12-06 08:55:36 -0800

    Include chai for real assertions

--- testrunner.js
+++ testrunner.js
@@ -1,14 +1,15 @@
 // A reusable web worker that runs tests.
 
 (function() {
+  importScripts('lib/chai-3.5.0.min.js');
 
-  // When the worker begins, record the set of global variables.
+  // When the worker begins, record the set of global variables (including chai).
   let properGlobals = new Set(Object.keys(self));
 
   // Removes all globals that were user-added, so the worker can be reused.
   function deleteNonProperGlobals() {
-    for (key in Object.keys(self)) {
-      if (!(key in properGlobals)) {
+    for (let key of Object.keys(self)) {
+      if (!(properGlobals.has(key))) {
         delete self[key];
       }
     }
@@ -16,8 +17,9 @@
 
   self.addEventListener('message', testCode => {
     try {
+      // Each test begins with the global object untarnished with previous mutations.
       deleteNonProperGlobals();
-      // TODO: Bundle an explanation with the result if the test fails
+      // TODO: Don't just return a boolean; bundle the failure reason if any
       self.postMessage(!!eval(testCode.data));
     } catch (e) {
       // TODO: Bundle the exception text with the result
